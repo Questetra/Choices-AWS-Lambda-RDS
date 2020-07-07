0db1aede7c968d8703aa367b8c56d2b7e42cab54
tatsumi <tatsumi@questetra.com>
2020-07-07 11:29:50 +0900

    パラメータグループを作成

--- database.tf
+++ database.tf
@@ -22,3 +22,40 @@ resource "aws_db_instance" "sample-database-1-instance-1" {
     performance_insights_enabled = true
     skip_final_snapshot       = true
 }
+
+resource "aws_rds_cluster_parameter_group" "paramgroup-for-utf8" {
+    description = "DB Cluster Parameter Group for UTF-8 Encoding"
+    family      = "aurora5.6"
+    name        = "paramgroup-for-utf8"
+
+    parameter {
+        apply_method = "immediate"
+        name         = "character_set_client"
+        value        = "utf8"
+    }
+    parameter {
+        apply_method = "immediate"
+        name         = "character_set_connection"
+        value        = "utf8"
+    }
+    parameter {
+        apply_method = "immediate"
+        name         = "character_set_database"
+        value        = "utf8"
+    }
+    parameter {
+        apply_method = "immediate"
+        name         = "character_set_results"
+        value        = "utf8"
+    }
+    parameter {
+        apply_method = "immediate"
+        name         = "character_set_server"
+        value        = "utf8"
+    }
+    parameter {
+        apply_method = "immediate"
+        name         = "skip-character-set-client-handshake"
+        value        = "1"
+    }
+}
